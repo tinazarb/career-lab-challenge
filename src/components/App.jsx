723129296d6037b66a92cfb3747ff35a229563fc
tinazarb <tinazarb@gmail.com>
2023-03-13 17:47:42 -0400

    render result list on app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,11 @@ import './App.css';
 import { searchArtworks } from '../utils/api';
 import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
+import { useState } from 'react';
 
 export function App() {
+	const [results, setResults] = useState();
+
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -13,7 +16,8 @@ export function App() {
 		// our UI, we need to make real requests!
 		// @see: ./src/uitls/api.js
 		searchArtworks(query).then((json) => {
-			console.log(json);
+			setResults(json.data);
+			console.log(json.data);
 		});
 	}
 
@@ -21,6 +25,24 @@ export function App() {
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
+			{!results ? (
+				<></>
+			) : (
+				results.map((result) => (
+					<>
+						<label key={result.id}>
+							<h4> Name: {result.title}</h4>
+							<h5>
+								Artist:{' '}
+								{result.artist_title === null
+									? '(Artist unknown)'
+									: result.artist_title}
+							</h5>
+							<h6>{result.date_display}</h6>
+						</label>
+					</>
+				))
+			)}
 			<Footer />
 		</div>
 	);
